e9c72b37415025b6d6dca4637ce0cbf8ddac41a6
Katy Currie <katylcurrie@gmail.com>
2021-04-23 20:58:56 +0100

    React testing lab for PDA

---
+++ cypress/integration/App.spec.js
@@ -0,0 +1,45 @@
+describe("Calculator", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+
+  it('should have working number buttons', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2')
+  })
+
+  it('should display the result of an arithmetical operation', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();    
+    cy.get('#operator_equals').click();  
+    cy.get('.display').should('contain', '6')
+  })
+
+  it('should return negative when subtracting larger number from smaller number', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-5')
+  })
+
+  it('should return decimal if numerator does not equally divide by denominator', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number4').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1.25')
+  })
+
+  it('should return 0 if number is divided by 0', () => {
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();    
+    cy.get('#number0').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '0')
+  })
+
+})
+
---
+++ src/tests/App.test.js
@@ -0,0 +1,85 @@
+import { fireEvent, render } from '@testing-library/react';
+import { run } from 'cypress';
+import App from '../App';
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = render(<App/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.getByTestId('number4');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button4);
+    expect(runningTotal).toHaveTextContent('4'); 
+  })
+
+  it('should change running total on adding of 1 and 4', () => {
+    const button1 = container.getByTestId('number1');
+    const button4 = container.getByTestId('number4');
+    const add = container.getByTestId('add');
+    const equals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button4);
+    fireEvent.click(equals);
+    expect(runningTotal).toHaveTextContent('5');
+  })
+
+  it('should change running total on subtracting of 4 from 7', () => {
+    const button7 = container.getByTestId('number7');
+    const button4 = container.getByTestId('number4');
+    const subtract = container.getByTestId('subtract');
+    const equals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button7);
+    fireEvent.click(subtract);
+    fireEvent.click(button4);
+    fireEvent.click(equals);
+    expect(runningTotal).toHaveTextContent('3');
+  })
+
+  it('should change running total on multiplication of 3 by 5', () => {
+    const button3 = container.getByTestId('number3');
+    const multiply = container.getByTestId('multiply');
+    const button5 = container.getByTestId('number5');
+    const equals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button3);
+    fireEvent.click(multiply);
+    fireEvent.click(button5);
+    fireEvent.click(equals);
+    expect(runningTotal).toHaveTextContent('15');
+  })
+
+  it('should change running total on dividing of 21 by 7', () => {
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const divide = container.getByTestId('divide');
+    const button7 = container.getByTestId('number7');
+    const equals = container.getByTestId('equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(divide);
+    fireEvent.click(button7);
+    fireEvent.click(equals);
+    expect(runningTotal).toHaveTextContent('3');
+  })
+
+  it('should concatenate multiple number button clicks', () => {
+    const button2 = container.getByTestId('number2');
+    const button1 = container.getByTestId('number1');
+    const button7 = container.getByTestId('number7'); 
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(button7);
+    fireEvent.click(button1);
+    expect(runningTotal).toHaveTextContent('2171');
+  })
+
+})
+
